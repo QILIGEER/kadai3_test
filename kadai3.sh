0dda1a68e9d6a69654d661494d4279f2173eb0f5
QILIGEER <qiligeer0313@gmail.com>
2017-06-01 19:32:41 +0900

    changed to kadai3.sh

--- kadai3.sh
+++ kadai3.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 # arguments check
-if [ -z $1 -o -z $2 ]; then
+if [ $# -lt 1 ]; then
   echo "kadai3.sh に2つの自然数入力してください" 1>&2
+exit 1
+  echo $1
+  exit 0
+fi
+
+if [ "$1" -gt "$2" ]; then
+  expr $1 - $2 | cat
+  exit 0
+else
+  echo error 1>&2
 exit 1
 fi
 
@@ -9,12 +19,6 @@ a=$1
 b=$2
 
 # preparation
-if [$a -lt $b]
-then
-  c=$a
-  a=$b
-  b=$c
-fi
 
 c=$((a%b))
 
