f8729c717524b88b619f4e7b4f0acaf690e4df44
QILIGEER <qiligeer0313@gmail.com>
2017-06-01 20:34:25 +0900

    changed to kadai3.sh to test_kadai3.sh
    - a test function in kadai3.sh was deleted
    - test_kadai3.sh was debugged

--- kadai3.sh
+++ kadai3.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
-# arguments check
-if [ $# -lt 1 ]; then
+
+## arguments check
+# input number check
+if [ $# -lt 2 ]; then
   echo "kadai3.sh に2つの自然数入力してください" 1>&2
-exit 1
-  echo $1
-  exit 0
+  exit 1
 fi
-
-if [ "$1" -gt "$2" ]; then
-  expr $1 - $2 | cat
-  exit 0
-else
-  echo error 1>&2
-exit 1
-fi
-
 a=$1
 b=$2
 
--- test_kadai3.sh
+++ test_kadai3.sh
@@ -10,32 +10,32 @@ ERROR_EXIT () {
 #########
 # TEST1 #
 #########
-echo 2 > $tmp-ans
-./test_kadai3.sh 4 6 > $tmp-out || ERROR_EXIT "TEST1-1"
+echo "sssss: 2" > $tmp-ans
+./kadai3.sh 4 6 > $tmp-out || ERROR_EXIT "TEST1-1"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
 
 echo test1 OK
 #########
 # TEST2 #
 #########
-echo 6 > $tmp-ans
-./test_kadai3.sh 24 18 > $tmp-out || ERROR_EXIT "TEST2-1"
+echo "sssss: 6" > $tmp-ans
+./kadai3.sh 24 18 > $tmp-out || ERROR_EXIT "TEST2-1"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST2-2"
 
 echo test2 OK
 #########
 # TEST3 #
 #########
-echo 1 > $tmp-ans
-./test_kadai3.sh 1 2 $tmp-out || ERROR_EXIT "TEST3-1"
+echo "sssss: 1" > $tmp-ans
+./kadai3.sh 1 2 > $tmp-out || ERROR_EXIT "TEST3-1"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST3-2"
 
 echo test3 OK
 #########
 # TEST4 #
 #########
-echo 2 > $tmp-ans
-./test_kadai3.sh 2 2 > $tmp-out || ERROR_EXIT "TEST4-1"
+echo "sssss: 2" > $tmp-ans
+./kadai3.sh 2 2 > $tmp-out || ERROR_EXIT "TEST4-1"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST4-2"
 
 echo test4 OK
